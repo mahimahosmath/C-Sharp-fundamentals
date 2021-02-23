b64d73b5704f706b1285ee4195c25261b11bcc0f
mahimahosmath <47314861+mahimahosmath@users.noreply.github.com>
2021-02-23 10:12:33 +0530

    Add files via upload

---
+++ LinkedListImplementation/LinkedListImplementation/Node.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListImplementation
+{
+    class Node
+    {   
+        public Node Next;
+            public object Value;
+       
+    }
+
+    public class LinkedList
+    {
+        private Node head;
+        private Node current;
+        public int Count;
+       
+
+
+        public LinkedList()
+        {
+            head = new Node();
+            current = head;
+
+        }
+
+        public void AddAtLast(object data)
+        {
+            Node newNode = new Node();
+            newNode.Value = data;
+            current.Next = newNode;
+            current = newNode;
+            Count++;
+        }
+        public void AddAtStart(object data)
+        {
+            Node newNode = new Node() { Value = data };
+            newNode.Next = head.Next;
+            head.Next = newNode;
+            Count++;
+        }
+
+        public void RemoveFromStart()
+        {
+            if (Count > 0)
+            {
+                head.Next = head.Next.Next;
+                Count--;
+            }
+            else
+            {
+                Console.WriteLine("No element exist in this linked list.");
+            }
+        }
+
+        public void RemoveFromLast()
+        {
+            if (Count > 0)
+            {
+                head.Next = head.Next.Next;
+                Count--;
+            }
+            else
+            {
+                Console.WriteLine("No element exist in this linked list.");
+            }
+        }
+
+
+
+        public void PrintAllNodes()
+        {
+            
+            Console.Write("Head ->");
+            Node curr = head;
+            while (curr.Next != null)
+            {
+                curr = curr.Next;
+                Console.Write(curr.Value);
+                Console.Write("->");
+            }
+            Console.Write("NULL");
+        }
+    }
+}
---
+++ LinkedListImplementation/LinkedListImplementation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LinkedListImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+                LinkedList ll = new LinkedList();
+                ll.PrintAllNodes();
+                Console.WriteLine();
+
+                ll.AddAtLast(12);
+                ll.AddAtLast("John");
+                ll.AddAtLast("Peter");
+                ll.AddAtLast(34);
+                ll.PrintAllNodes();
+               Console.WriteLine();
+
+                ll.AddAtStart(55);
+                ll.PrintAllNodes();
+                Console.WriteLine();
+
+                ll.RemoveFromStart();
+                ll.PrintAllNodes();
+
+                Console.ReadKey();
+       
+        }
+    }
+}
